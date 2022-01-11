b4893fc88c3623d43bfffc0a0981126cbf7a37ac
gustave mary <gustavemary@MacBook-Pro-de-gustave.local>
2022-01-11 15:55:53 +0100

    all working great, check if leaks ok?

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: gmary <gmary@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/10 10:16:25 by gmary             #+#    #+#             */
-/*   Updated: 2022/01/11 15:37:41 by gmary            ###   ########.fr       */
+/*   Updated: 2022/01/11 15:53:38 by gmary            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,7 @@ void	ft_putstr_fd_modif(char *s, int fd)
 		write(fd, &s[i], 1);
 		i++;
 	}
+	free(s);
 }
 
 char	*ft_strjoin_modif(char *s1, char s2)
@@ -58,112 +59,29 @@ char	*ft_strjoin_modif(char *s1, char s2)
 void	ft_sig_handler(int	sig, siginfo_t *details, void *nothing)
 {
 	static char	current;
-	static int		bits = 7;
+	static int	bits = 7;
 	static char	*str;
-	//static int		i;
-	//static	char	str[1000];
+
 	(void)details;
 	(void)nothing;
-	
-	//i = 0;
-	//current = NULL;
 	if (sig == SIGUSR1)
 		current |= (1 << bits);
-		//str[i] |= (1 << bits);
 	if (sig == SIGUSR2)
 		current &= ~(1 << bits);
-		//str[i] &= ~(1 << bits);
 	bits--;
 	if (bits == -1)
 	{
-		//printf("ENTER_____");
-		//printf("%c\n", str[i]);
-//		if (!str)
-//			str = ft_strdup("");
 		str = ft_strjoin_modif(str, current);
 		bits = 7;
-		//if (str[i] == '\0')
-		printf("|-------------------------");
 		if (current == '\0')
 		{
-			printf("|-------REELLL------------------");
 			ft_putstr_fd_modif(str, 1);
+			str = NULL;
+			current = 0;
 			//free(str);
 		}
-		//else 
-		//	i++;
-		//printf("i =%d\n", i);
-	}
-	
-	
-	
-	
-	
-	
-	//static char	buf[10000] = {'\0', };
-	//static int	bit = 7;
-	//static int	i = 0;
-//
-	//if (sig == SIGUSR1)
-	//	buf[i] |= (1 << bit);
-	//if (sig == SIGUSR2)
-	//	buf[i] &= ~(1 << bit);
-	//bit--;
-	//if (bit == -1)
-	//{
-	//	bit = 7;
-	//	if (buf[i] == '\0')
-	//	{
-	//		write(1, buf, i);
-	//		i = 0;
-	//	}
-	//	else
-	//	{
-	//		i++;
-	//	}
-	//}
-
-		//str = malloc(sizeof(char));
-		//str[ft_strlen(str)] = current;
-		//if (current == '\0')
-		//{
-		//	ft_putstr_fd(str, 1);
-		//	free(str);
-		//}
-		//bits = 8;
-		//current = '\0';
-	
-}
-
-/*
-void	ft_sig_handler(int	sig, siginfo_t *details, void *nothing)
-{
-	static	char	current;
-	static	int		bits = 7;
-	static	char	*str;
-	(void)details;
-	(void)nothing;
-
-	current = NULL;
-	if (sig == SIGUSR1)
-		current |= (1 << bits);
-	bits--;
-	if (bits == -1)
-	{
-		str = malloc(sizeof(char) * ft_strlen(str) + 1);
-		str[ft_strlen(str)] = current;
-		if (current == '\0')
-		{
-			ft_putstr_fd(str, 1);
-			free(str);
-		}
-		bits = 8;
-		current = '\0';
 	}
 }
-*/
-
-
 /* 
 Si SA_SIGINFO est indiqué dans sa_flags, sa_sigaction (plutôt que sa_handler)
 indique la fonction gestionnaire de signaux pour signum.
@@ -192,9 +110,6 @@ int	main(void)
 	sigaction(SIGUSR1, &action, NULL);
 	sigaction(SIGUSR2, &action, NULL);
 
-	//while (message pas finit)
-	//	while (tant que le char est pas finit )
-	//		while(tant que jai pas recu de signal)
 	while (1)
 		pause();
 	return (0);
